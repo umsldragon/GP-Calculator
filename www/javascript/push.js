e734f1fcf0975a7a001360c2adc7d57a15e450bd
umsldragon <a1dotz9@yahoo.ca>
2018-12-25 19:00:46 -0600

    Update push.js
    added an alert to show my the devices ID

--- www/javascript/push.js
+++ www/javascript/push.js
@@ -2,12 +2,13 @@
 e_yo0B57o90:APA91bH4umqRBqG3AiM5stvOA4KNvf8zQqnSD8fa9sYwzuiZ7yM2LMsS9IJkPw8WQOw5EhXFh5SdtTi6VXaHbFFkkozo3UPx7Pgn-LAlTyugpWuvPFkWaApJii_iAgMVwLX5wtd_D7s4VanTh8whO7aZSst-Sc7tbQ
 */
 $(function(){
-alert('Start');
+	alert('Start');
 	var push = PushNotification.init({ "android": {"senderID": "AIzaSyCl_HH_SctsH_--moo7VxCEIb4QsTxTQq4"},
          "ios": {"alert": "true", "badge": "true", "sound": "true"}, "windows": {} } );
 
     push.on('registration', function(data) {
 		// register ID to server: data.registrationId
+		alert(data.registrationId);
     });
 
     push.on('notification', function(data) {
